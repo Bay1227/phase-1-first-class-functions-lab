fd467d8ee1e2dc4b9f3069a1a8a0f6dcc2432d14
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-07-22 13:00:55 -0700

    Completed Advanced First-Class Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+function returnFirstTwoDrivers() {
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers() {
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(a) {
+    return function (b) { 
+        return a * b;
+    }
+}
+function fareDoubler(a) {
+    return a * 2;
+}
+function fareTripler(a) {
+    return a * 3;
+}
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
